8f1edfa8f51b9e0b6ab4a72837ab0ec0f56bece6
kris.nguyen <kris.nguyen@avepoint.com>
2023-08-08 11:34:27 +0700

    Update Validate

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Image from "../../Images/Image";
 import "./index.scss";
@@ -18,11 +18,23 @@ function LoginPage() {
     const userAuth = useUserStore();
 
     const handleValidation = () => {
-        if (!username || !password) {
+        if (!username.trim() || !password.trim()) {
             toast.error("Please fill in all fields.");
             return false;
         }
-        if (password.length < 8) {
+        if (username.trim().length < 5) {
+            toast.error("Username must be at least 5 characters long");
+            return false;
+        }
+        if (/\s/.test(username.trim())) {
+            toast.error("Username cannot contain spaces");
+            return false;
+        }
+        if (/[^a-zA-Z0-9.]/.test(username.trim())) {
+            toast.error("Username can only contain English letters, numbers, and .");
+            return false;
+        }
+        if (password.trim().length < 8) {
             toast.error("Password must be at least 8 characters long.");
             return false;
         }
@@ -43,32 +55,40 @@ function LoginPage() {
                 dispatchEvent(new Event("storage"));
                 navigate("/upcomming");
             } else if (result.status === StatusType.Failed) {
-                toast.error(result.description)
+                toast.error(result.description);
             }
         });
     };
 
     return (
-        <div style={{ backgroundImage: `url(${Image.login_bgr})` }} className="login__form">
-            <div className="form__container">
-                <form action="">
-                    <h1 className="title">Sign-in</h1>
-                    <input value={username} onChange={(e) => setUsername(e.target.value)} className="user_email" type="text" placeholder="username" />
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} className="user_password" type="password" placeholder="Password" />
+        <div className="outter__form--container">
+            <div style={{ backgroundImage: `url(${Image.login_bgr})` }} className="login__form">
+                <div className="form__container">
+                    <form action="">
+                        <h1 className="title">Sign-in</h1>
+                        <input value={username} onChange={(e) => setUsername(e.target.value)} className="user_email" type="text" placeholder="username" />
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="user_password"
+                            type="password"
+                            placeholder="Password"
+                        />
 
-                    <div className="submit__btn--container">
-                        <button type="submit" onClick={handleLogIn}>
-                            Sign in
-                        </button>
-                    </div>
-                    <div className="form__extra">
-                        <Link>Forgot your password?</Link>
-                    </div>
-                    <div className="form__extra">
-                        <span>Don't have an account?</span>
-                        <Link to={"/register"}>Sign up</Link>
-                    </div>
-                </form>
+                        <div className="submit__btn--container">
+                            <button type="submit" onClick={handleLogIn}>
+                                Sign in
+                            </button>
+                        </div>
+                        <div className="form__extra">
+                            <Link>Forgot your password?</Link>
+                        </div>
+                        <div className="form__extra">
+                            <span>Don't have an account?</span>
+                            <Link to={"/register"}>Sign up</Link>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -11,6 +11,7 @@ function RegisterPage() {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [facebookLink, setFacebookLink] = useState("");
+    const [facebookName, setFacebookName] = useState("");
     const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
     const [retypePassword, setRetypePassword] = useState("");
@@ -20,28 +21,43 @@ function RegisterPage() {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     const handleValidation = () => {
-        if (!email || !username || !facebookLink || !phone || !password || !retypePassword) {
+        if (!email.trim() || !username.trim() || !facebookLink.trim() || !phone.trim() || !password.trim() || !retypePassword.trim() || !facebookName.trim()) {
             toast.error("Please fill in all fields.");
             return false;
         }
-
-        if (!emailRegex.test(email)) {
-            toast.error("Please enter a valid email address.");
+        if (!emailRegex.test(email.trim())) {
+            toast.error("Please enter a valid email address");
+            return false;
+        }
+        if (username.trim().length < 5) {
+            toast.error("Username must be at least 5 characters long");
+            return false;
+        }
+        if (/\s/.test(username.trim())) {
+            toast.error("Username cannot contain spaces");
+            return false;
+        }
+        if (/[^a-zA-Z0-9.]/.test(username.trim())) {
+            toast.error("Username can only contain English letters, numbers, and .");
             return false;
         }
 
-        if (password.length < 8) {
+        if (!/^(https?:\/\/)?(www\.)?facebook\.com\/[a-zA-Z0-9.]+\/?$/i.test(facebookLink.trim())) {
+            toast.error("Invalid Facebook link");
+            return false;
+        }
+        if (!/^(0|\+84)(3[2-9]|5[2689]|7[06-9]|8[1-689]|9[0-9])[0-9]{7}$/.test(phone)) {
+            toast.error("Invalid phone number");
+            return false;
+        }
+        if (password.trim().length < 8) {
             toast.error("Password must be at least 8 characters long.");
             return false;
         }
-
-        if (password !== retypePassword) {
-            toast.error("Passwords do not match.");
+        if (password.trim() !== retypePassword.trim()) {
+            toast.error("Passwords do not match");
             return false;
         }
-
-        // Additional validation checks can be added based on your requirements
-
         return true;
     };
 
@@ -67,39 +83,54 @@ function RegisterPage() {
     };
 
     return (
-        <div style={{ backgroundImage: `url(${Image.login_bgr})` }} className="register__form">
-            <div className="form__container">
-                <form action="">
-                    <h1 className="title">Register</h1>
-                    <input value={email} className="user_email" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                    <input value={username} className="user_password" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-                    <input
-                        value={facebookLink}
-                        className="user_password"
-                        type="text"
-                        placeholder="Facebook link"
-                        onChange={(e) => setFacebookLink(e.target.value)}
-                    />
-                    <input value={phone} className="user_password" type="text" placeholder="Phone" onChange={(e) => setPhone(e.target.value)} />
-                    <input value={password} className="user_password" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                    <input
-                        value={retypePassword}
-                        className="user_password"
-                        type="password"
-                        placeholder="Retype Password"
-                        onChange={(e) => setRetypePassword(e.target.value)}
-                    />
+        <div className="outter__form--container">
+            <div style={{ backgroundImage: `url(${Image.login_bgr})` }} className="register__form">
+                <div className="form__container">
+                    <form action="">
+                        <h1 className="title">Register</h1>
+                        <input value={email} className="user_email" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                        <input value={username} className="user_password" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+                        <input
+                            value={facebookName}
+                            className="user_password"
+                            type="text"
+                            placeholder="Display name"
+                            onChange={(e) => setFacebookName(e.target.value)}
+                        />
+                        <input
+                            value={facebookLink}
+                            className="user_password"
+                            type="text"
+                            placeholder="Facebook link"
+                            onChange={(e) => setFacebookLink(e.target.value)}
+                        />
+                        <input value={phone} className="user_password" type="text" placeholder="Phone" onChange={(e) => setPhone(e.target.value)} />
+                        <input
+                            value={password}
+                            className="user_password"
+                            type="password"
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <input
+                            value={retypePassword}
+                            className="user_password"
+                            type="password"
+                            placeholder="Retype Password"
+                            onChange={(e) => setRetypePassword(e.target.value)}
+                        />
 
-                    <div className="submit__btn--container">
-                        <button type="submit" onClick={handleRegister}>
-                            Register
-                        </button>
-                    </div>
-                    <div className="form__extra">
-                        <span>Already had an account?</span>
-                        <Link to="/login">Log in</Link>
-                    </div>
-                </form>
+                        <div className="submit__btn--container">
+                            <button type="submit" onClick={handleRegister}>
+                                Register
+                            </button>
+                        </div>
+                        <div className="form__extra">
+                            <span>Already had an account?</span>
+                            <Link to="/login">Log in</Link>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
